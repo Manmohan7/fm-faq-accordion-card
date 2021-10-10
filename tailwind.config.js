8424f9b03ed6409809fce0a46c7290807281b69e
Manmohan Singh <er.manmohansingh7@gmail.com>
2021-10-10 20:49:30 +0530

    colors update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       colors: {
-        pink: '#D65DB1',
-        purple: '#845EC2'
+        blue: {
+          dark: 'hsl(237, 12%, 33%)',
+          DEFAULT: 'hsl(240, 6%, 50%)',
+          light: 'hsl(240, 5%, 91%)',
+          soft: 'hsl(240, 73%, 65%)'
+        },
+        desaturated: 'hsl(238, 29%, 16%)',
+        red: 'hsl(14, 88%, 65%)',
+        violet: 'hsl(273, 75%, 66%)',
       },
       rotate: {
         '225': '225deg'
